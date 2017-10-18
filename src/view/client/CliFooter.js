f61763d23a0eae5c9345775c77f19c8b7845509d
Ozhmegov Egor <egor-ozhmegov@mail.ru>
2017-10-18 17:40:07 +0300

    added phone and gmail icons

--- src/view/client/CliFooter.js
+++ src/view/client/CliFooter.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import '../css/CliFooter.css';
 import { Link } from 'react-router-dom';
+import gmail from '../img/gmail.svg';
+import phone from '../img/telephone.svg';
 
 export default class CliFooter extends Component {
     render() {
@@ -9,8 +11,10 @@ export default class CliFooter extends Component {
                 <ul className="footer">
                     <li><img className="user-profile" src={this.props.user.photoURL} alt="google-foto"/></li>
                     <li className="cli-footer-li">{this.props.user.displayName}</li>
-                    <li className="cli-footer-li-right">Email: <Link to='#'>info@rws.com</Link></li>
-                    <li className="cli-footer-li-right">Phone: +7(812) 356-12-42</li>
+                    <li className="cli-footer-li-right"><Link to='#'>rws.inf@gmail.com</Link></li>
+                    <li className="gmail-logo"><img src={gmail} alt="gmail_logo" /></li>
+                    <li className="cli-footer-li-right">+7 (812) 356-12-42</li>
+                    <li className="phone-logo"><img src={phone} alt="phone_logo" /></li>
                 </ul>
             </div>
         );
